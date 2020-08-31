e46b929b9d51c710eefceb4f2e4c394bf9d6bcff
夏爽 <996190264@qq.com>
2020-08-31 10:41:29 +0800

    add phpunit tests

---
+++ src/tests/TestCollection.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Xs\tests;
+
+
+use PHPUnit\Framework\TestCase;
+use Xs\Collection;
+
+class TestCollection extends TestCase
+{
+    public function testColumn()
+    {
+        $data = [
+            ['a'=>1,'b'=>2],
+            ['a'=>2,'b'=>3],
+        ];
+        $data = Collection::make($data)->colunm('a')->toArray();
+        $this->assertEquals([1,2],$data);
+    }
+
+
+    public function testSum()
+    {
+        $data = [
+            ['a' => 1, 'b' => 2],
+            ['a' => 2, 'b' => 3],
+        ];
+        $sum = Collection::make($data)->sum('a');
+        $this->assertEquals(3, $sum);
+    }
+
+
+    public function testAvg()
+    {
+        $data = [
+            ['a' => 1, 'b' => 2],
+            ['a' => 2, 'b' => 6],
+        ];
+        $sum = Collection::make($data)->avg('b');
+        $this->assertEquals(4, $sum);
+    }
+}
