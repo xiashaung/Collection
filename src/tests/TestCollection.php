1b5dc8fa3fcdee217ab79c2077ea1053ae765535
夏爽 <996190264@qq.com>
2020-11-03 16:41:27 +0800

    fix test

--- src/tests/TestCollection.php
+++ src/tests/TestCollection.php
@@ -9,35 +9,46 @@ use Xs\Collection;
 
 class TestCollection extends TestCase
 {
-    public function testColumn()
+
+    public function getCollection()
     {
         $data = [
-            ['a'=>1,'b'=>2],
-            ['a'=>2,'b'=>3],
+            ['a' => 1, 'b' => 2],
+            ['a' => 2, 'b' => 3],
         ];
-        $data = Collection::make($data)->column('a')->toArray();
+        return Collection::make($data);
+    }
+
+
+    public function testColumn()
+    {
+        $data = $this->getCollection()->column('a')->toArray();
         $this->assertEquals([1,2],$data);
     }
 
 
     public function testSum()
     {
-        $data = [
-            ['a' => 1, 'b' => 2],
-            ['a' => 2, 'b' => 3],
-        ];
-        $sum = Collection::make($data)->sum('a');
-        $this->assertEquals(3, $sum);
+        $data = $this->getCollection()->sum('a');
+        $this->assertEquals(3, $data);
     }
 
 
     public function testAvg()
     {
-        $data = [
-            ['a' => 1, 'b' => 2],
-            ['a' => 2, 'b' => 6],
-        ];
-        $sum = Collection::make($data)->avg('b');
-        $this->assertEquals(4, $sum);
+        $data = $this->getCollection()->avg('b');
+        $this->assertEquals(2.5, $data);
+    }
+
+    public function testMin()
+    {
+        $min = $this->getCollection()->min('a');
+        $this->assertEquals(1, $min);
+    }
+
+    public function testMax()
+    {
+        $min = $this->getCollection()->max('b');
+        $this->assertEquals(3, $min);
     }
 }
